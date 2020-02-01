a04847e99b6632ac942c5656253990268b8bd035
Lars Burgwal <lars@modularmood.com>
2020-02-01 19:05:46 +0100

    2d + 3d audio test

---
+++ Assets/Scripts/FMOD/CityAmbience.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityAmbience : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string EventPath;
+    FMOD.Studio.EventInstance CityAmb;
+
+    Transform transformPos = null;
+
+    private void Awake()
+    {
+        CityAmb = FMODUnity.RuntimeManager.CreateInstance(EventPath);
+        CityAmb.start();
+        CityAmb.release();
+    }
+
+    private void Start()
+    {
+        transformPos = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        CityAmb.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transformPos));
+    }
+
+    private void OnDestroy()
+    {
+        CityAmb.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+}
