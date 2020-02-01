c1c68239e2c0bf1329e4e0a89bb578ddfe4c55f6
Lars Burgwal <lars@modularmood.com>
2020-02-01 18:17:31 +0100

    Added ambience script

---
+++ Assets/Scripts/FMOD/AmbiencePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiencePlayer : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string EventPath;
+    FMOD.Studio.EventInstance RoomTone;
+
+    private void Awake()
+    {
+        RoomTone = FMODUnity.RuntimeManager.CreateInstance(EventPath);
+        RoomTone.start();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        RoomTone.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+}
