0c39824fe454e80de44f4d7f5f88ed9aa3c05e7d
Lars Burgwal <lars@modularmood.com>
2020-02-01 20:55:17 +0100

    Added ToVoicemail trigger

--- Assets/Scripts/FMOD/PhoneAudio.cs
+++ Assets/Scripts/FMOD/PhoneAudio.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
 
 public class PhoneAudio : MonoBehaviour
 {
 
     [FMODUnity.EventRef]
-    public string EventPath;
-    //FMOD.Studio.EventInstance AlexCalling;
+    public string AlexCallingEvent;
+    FMOD.Studio.EventInstance AlexCalling;
+    public string OneNewVoicemailEvent;
+    FMOD.Studio.EventInstance OneNewVoicemail;
 
+    Transform transformPos = null;
 
     void Start()
     {
-        FMODUnity.RuntimeManager.PlayOneShotAttached(EventPath, gameObject);
+        transformPos = GetComponent<Transform>();
+
+        AlexCalling = FMODUnity.RuntimeManager.CreateInstance(AlexCallingEvent);
+        AlexCalling.start();
+        AlexCalling.release();
+        //FMODUnity.RuntimeManager.PlayOneShotAttached(EventPath, gameObject);
     }
 
 
 
     void Update()
     {
+        AlexCalling.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transformPos));
+
         if (Input.GetKey(KeyCode.V))
         {
+            Debug.Log("v pressed");
+            AlexCalling.setParameterByName("ToVoicemail", 1f);
 
         }
     }
