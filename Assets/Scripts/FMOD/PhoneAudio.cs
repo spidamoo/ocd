5423233ea02024ee4a10f31165fa9fa707c98000
Lars Burgwal <lars@modularmood.com>
2020-02-01 20:36:45 +0100

    Phone

---
+++ Assets/Scripts/FMOD/PhoneAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneAudio : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string EventPath;
+    //FMOD.Studio.EventInstance AlexCalling;
+
+
+    void Start()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(EventPath, gameObject);
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.V))
+        {
+
+        }
+    }
+}
