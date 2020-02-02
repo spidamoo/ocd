8ba29bb8940b887300c58fc06de30b9c7a8ae33d
Vlad Zatoloka <vladimir.zatoloka@booking.com>
2020-02-02 14:42:16 +0100

    starting scene

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    private Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(m_Animator.GetCurrentAnimatorClipInfo(0)[0].clip);
+        if (m_Animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "End titles")
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
